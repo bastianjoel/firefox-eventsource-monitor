c0dbc02c9c15826a0d7c0f54186ff7b3b5205762
Bastian Rihm <me@bastianjoel.de>
2024-11-07 12:03:20 +0100

    Fix data table column resize

--- devtools/event-sources-list/lib/resize.js
+++ devtools/event-sources-list/lib/resize.js
@@ -1,26 +1,34 @@
-const leftPane = document.querySelector(".left");
-const rightPane = document.querySelector(".right");
-const gutter = document.querySelector(".gutter");
+(function () {
+  const wrapper = document.querySelector(".pane-container");
+  const leftPane = document.querySelector(".left");
+  const rightPane = document.querySelector(".right");
+  const gutter = document.querySelector(".gutter");
 
-function resizer(e) {
-  window.addEventListener('mousemove', mousemove);
-  window.addEventListener('mouseup', mouseup);
-  
-  let prevX = e.x;
-  const leftPanel = leftPane.getBoundingClientRect();
-  
-  
-  function mousemove(e) {
-    let newX = prevX - e.x;
-    leftPane.style.width = leftPanel.width - newX + "px";
+  function resizer(e) {
+    e.preventDefault();
+    window.addEventListener('mousemove', mousemove);
+    window.addEventListener('mouseup', mouseup);
+    
+    let prevX = e.x;
+    const leftPanel = leftPane.getBoundingClientRect();
+    const wrapperSize = wrapper.getBoundingClientRect();
+    
+    
+    function mousemove(e) {
+      e.preventDefault();
+      const newX = prevX - e.x;
+      const newWidth = leftPanel.width - newX;
+      leftPane.style.width = newWidth + "px";
+      rightPane.style.width = wrapperSize.width - newWidth + "px";
+    }
+    
+    function mouseup() {
+      window.removeEventListener('mousemove', mousemove);
+      window.removeEventListener('mouseup', mouseup);
+
+      window.dispatchEvent(new Event(`pane-resize`));
+    }
   }
-  
-  function mouseup() {
-    window.removeEventListener('mousemove', mousemove);
-    window.removeEventListener('mouseup', mouseup);
 
-    window.dispatchEvent(new Event(`pane-resize`));
-  }
-}
-
-gutter.addEventListener('mousedown', resizer);
+  gutter.addEventListener('mousedown', resizer);
+})();
